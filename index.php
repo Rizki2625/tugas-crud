4e8719cc5319d6cb6ae76087bc70b4a19a1815fb
MUhammad Rizki <muhammadrizuki8@gmail.com>
2021-01-18 13:38:38 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+
+	<h1 class="text-center">CRUD</h1>
+	<h2 class="text-center"> Tugas CRUD</h2>
+
+	<!-- awal card form -->
+	<div class="card mt-3">
+	  <div class="card-header bg-primary text-white">
+	    Form input data mahasiswa
+	  </div>
+	  <div class="card-body">
+	    
+	  </div>
+	</div>
+	<!-- akhir form -->
+
+</div>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
